6d28e47fc87a8a942c7dade5ea1ef7ea5040b605
User <user@Userui-MacBookAir.local>
2023-09-27 06:05:21 +0900

    feat:make deps

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,8 +1,18 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 import baseConfig from "@acme/tailwind-config";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   presets: [baseConfig],
+  darkMode: "class",
+  plugins: [
+    nextui(),
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/typography"),
+  ],
 } satisfies Config;
